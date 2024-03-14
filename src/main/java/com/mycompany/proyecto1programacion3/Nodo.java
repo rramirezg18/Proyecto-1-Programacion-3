eddfb9af7f81e16d2fe6717f74c5d4821335afc7
rramirezg18 <rramirezg18@miumg.edu.gt>
2024-03-13 18:25:15 -0600

    first commit

---
+++ src/main/java/com/mycompany/proyecto1programacion3/Nodo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1programacion3;
+
+/* clase nodo con atributos constructor y metodos setters y getters
+ * Roberto Antonio Ramírez Gómez	7690-22-12700
+ * Jean Klaus Castañeda Santos		7690-22-892		
+ * Jonathan Joel Chan Cuellar		7690-22-1805
+ */
+public class Nodo {
+	private String dato;
+	private Nodo hijoDerecho;
+	private Nodo hijoIzquierdo;
+	
+	//Constructor
+	public Nodo (String dato) {
+		this.dato = dato;
+		this.hijoDerecho = null;
+		this.hijoIzquierdo = null;
+	}
+	//Setters y Getters
+	public String getDato() {
+		return dato;
+	}
+
+	public void setDato(String dato) {
+		this.dato = dato;
+	}
+
+	public Nodo getHijoDerecho() {
+		return hijoDerecho;
+	}
+
+	public void setHijoDerecho(Nodo hijoDerecho) {
+		this.hijoDerecho = hijoDerecho;
+	}
+
+	public Nodo getHijoIzquierdo() {
+		return hijoIzquierdo;
+	}
+
+	public void setHijoIzquierdo(Nodo hijoIzquierdo) {
+		this.hijoIzquierdo = hijoIzquierdo;
+	}
+
+}
---
+++ src/main/java/com/mycompany/proyecto1programacion3/Proyecto1Programacion3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1programacion3;
+
+/**
+ *
+ * @author ianto
+ */
+public class Proyecto1Programacion3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
