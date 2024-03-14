cb4cd2c27e6c48c856d8ff8f02bd94ed8a1605d4
jonathanc4chan <jchanc4@miumg.edu.gt>
2024-03-13 19:37:13 -0600

    primer commit jonathan

--- src/main/java/com/mycompany/proyecto1programacion3/Proyecto1Programacion3.java
+++ src/main/java/com/mycompany/proyecto1programacion3/Proyecto1Programacion3.java
@@ -13,4 +13,29 @@ public class Proyecto1Programacion3 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    private static boolean contieneVariables(String expresion) {
+        for (char c : expresion.toCharArray()) {
+            if (Character.isLetter(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+ 
+    // Método para obtener todas las variables de la expresión
+    private static char[] obtenerVariables(String expresion) {
+        String variables = expresion.replaceAll("[^a-z]", "");
+        return variables.toCharArray();
+    }
+    
+    private static boolean validarExpresionMatematica(String expresionMatematica) {
+        String caracteresValidos = "+-*/^(){}[]abcdefghijklmnopqrstuvwxyz0123456789";
+        for(char caracter : expresionMatematica.toCharArray()) {
+            if(caracteresValidos.indexOf(caracter) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
